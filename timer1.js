8007d9270388ee18ae634a6abe531307d2da2bed
William Li <110069721+william-li0128@users.noreply.github.com>
2022-10-16 10:53:41 -0700

    refactored

--- timer1.js
+++ timer1.js
@@ -1,13 +1,5 @@
 const timerList = process.argv.slice(2);
 
-const timerChecker = function(timer) {
- if (timer >= 0) {
-    console.log(typeof timer);
-    return true;
-  }
-  return false;
-};
-
 for (let timer of timerList) {
   timer = Number(timer);
   if (timer >= 0) {
